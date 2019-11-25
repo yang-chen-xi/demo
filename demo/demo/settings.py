240b330f8d40c06aad595a7bd93e55f0c2599362
yang-chen-xi <478463274@qq.com>
2019-11-25 20:53:49 +0800

    开发者A的修改

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '381#*0qf#@!v16nq54!d@fea7er58mhgu7_u174h(x-5o&4g8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
